5724a1c11ac4fa8d6a0abadec667a49b6967293d
hushenwei2000 <1223929119@qq.com>
2020-12-14 21:34:05 +0800

    add pics

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,7 +1,11 @@
 
 Page({
   data: {
-    
+    imgs: [
+      "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2522069454.jpg",
+      "https://img1.doubanio.com/view/photo/s_ratio_poster/public/p2522778567.jpg",
+      "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2523516430.jpg",
+    ],
   },
   //事件处理函数
   bindViewTap: function() {
